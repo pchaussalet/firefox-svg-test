444e6c84f1882bd57886f99827b49d66c46122ae
Joshua Rule Dobson <noreply>
2015-02-05 23:03:52 +0000

    Add component svg

---
+++ ui/svg.reel/svg.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/svg.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Svg
+ * @extends Component
+ */
+exports.Svg = Component.specialize(/** @lends Svg# */ {
+    constructor: {
+        value: function Svg() {
+            this.super();
+        }
+    }
+});
